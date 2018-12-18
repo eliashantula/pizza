494da9e1ed936c3a7a3026ca10a6b975142d7001
Elias Hantula <ehantula@indiana.edu>
2018-12-18 16:45:39 -0500

    uploaded

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ import hawaian from "./hawaian.jpg";
 import mushroom from "./mushroom.jpg";
 import veggie from "./veggie.jpg";
 import cheese from "./Cheese.jpg";
-import Intro from  "./intro"
-import Navs from './Nav'
-import white from './white.jpg'
+import Intro from "./intro";
+import Navs from "./Nav";
+import white from "./white.jpg";
 const pizzas = [
   {
-    image: pepperoni ,
+    image: pepperoni,
     price: "5.00",
     name: "Pepperoni Pizza",
     calories: "229 calories per slice"
@@ -24,35 +24,49 @@ const pizzas = [
     calories: "274 calories per slice"
   },
   {
-    image: mushroom ,
+    image: mushroom,
     price: 3.75,
     name: "Double-Mushroom Pizza",
     calories: "365 calories per slice"
   },
   {
-    image: hawaian ,
+    image: hawaian,
     name: "Hawaiian Pizza",
     price: 6.75,
     calories: "450 calories per slice"
-  },{
-  image: cheese,
-  price: "2.50",
-  name: "Cheese Pizza",
-  calories: "100 calories per slice"
-}
+  },
+  {
+    image: cheese,
+    price: "2.50",
+    name: "Cheese Pizza",
+    calories: "100 calories per slice"
+  }
 ];
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{backgroundColor: "white"}}>
-      <Intro/>
-      <Navs/>
+      <div className="App" style={{ backgroundColor: "white" }}>
+        <Intro />
+        <Navs />
         <div className="container">
-
-          <div className="main" style={{backgroundImage: `url(${white})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", backgroundPosition: "center", backgroundColor: "", borderRadius: "15px", padding: "20px", border: "1px solid black"}}>
-            <section> <a href="menu"> </a>
-            <PizzaMenu pizzas = {pizzas}   />
+          <div
+            className="main"
+            style={{
+              backgroundImage: `url(${white})`,
+              backgroundRepeat: "no-repeat",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              backgroundColor: "",
+              borderRadius: "15px",
+              padding: "20px",
+              border: "1px solid black"
+            }}
+          >
+            <section>
+              {" "}
+              <a href="menu"> </a>
+              <PizzaMenu pizzas={pizzas} />
             </section>
           </div>
           <footer>
--- src/PizzaMenu.js
+++ src/PizzaMenu.js
@@ -8,14 +8,19 @@ const Pizza = ({ pizzas }) => {
 		return (
 			<Col sm="12" style={{ margin: "10px" }}>
 				<div className="pizzaItem">
-					<h4 className="title" style={{ paddingBottom: "0px" }}>
+					<h4 className="title" style={{ paddingBottom: "5px" }}>
 						{pizza.name}
 					</h4>
 					<img src={pizza.image} className="rounded img" />{" "}
-					<h5 className="price" style={{ paddingTop: "5px", fontSize: "30px" }}>
+					<h5
+						className="price"
+						style={{ paddingTop: "10px", fontSize: "30px" }}
+					>
 						${pizza.price}
 					</h5>
-					<h6 className="calories" style={{fontSize:"15px"}}>{pizza.calories}</h6>
+					<h6 className="calories" style={{ fontSize: "15px" }}>
+						{pizza.calories}
+					</h6>
 				</div>
 			</Col>
 		);
@@ -26,7 +31,7 @@ class PizzaMenu extends Component {
 	render() {
 		const { pizzas } = this.props;
 		return (
-			<section id="menu">
+			<section id="menu" style={{ paddingTop: "75px" }}>
 				<Container>
 					<Row>
 						<Pizza pizzas={pizzas} />
